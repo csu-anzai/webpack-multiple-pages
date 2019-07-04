c1a3be27acb6a6360a14880a1961c5660c5d2b5e
Dongyi An <acmilank22@gmail.com>
2019-07-04 10:18:46 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    'shared-node-browser': true,
+    es6: true,
+    worker: true,
+    amd: true
+  },
   extends: ['airbnb-base', 'plugin:prettier/recommended']
 };
